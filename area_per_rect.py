d9607756b98cf85410fdbe703181de76a53410fa
Noah-234 <noah.ouellette@stu.ocsb.ca>
2021-11-26 05:05:25 +0000

    Created a program to determine the area and perimeter of a rectangle with specific dimensions.

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By: Noah Ouellette
+# Date: Nov. 24, 2021
+# This program calculates the area and perimeter of a rectangle with
+# the dimensions of 3456cm by 5873cm.
+
+
+def main():
+    # This function calculates the area and perimeter.
+    print("If a rectangle has the dimensions of 3456cm and 5873cm:")
+    print("")
+
+    print("The area would be {}cm^2".format(3456*5873) + " and the perimeter" +
+          " would be {}cm.".format(2*(3456+5873)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Noah Ouellette
+# Date: Nov. 24, 2021
+# This program performs very basic math operations.
+
+
+def main():
+    # This function performs and prints the basic math operations.
+    print("76+5={}".format(76+5))
+    print("334-978={}".format(334-978))
+    print("34*23={}".format(34*23))
+    print("912/385={}".format(912/385))
+    print("234^6={}".format(234**6))
+
+
+if __name__ == "__main__":
+    main()
